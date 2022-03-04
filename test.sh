ff020a2553760d3c975bec221d32e9056e676311
Akaifox16 <kamonpat_s@cmu.ac.th>
2022-03-05 01:09:11 +0700

    add loging in automation

--- test.sh
+++ test.sh
@@ -6,16 +6,21 @@ cmake ..
 
 make
 
+echo "\nassembler working on assembly"
 ./Assembler ../test/assembly ../test/machinecode/machine
+echo "\nassembler working on factorial"
 ./Assembler ../test/factorial ../test/machinecode/factorial_machine
+echo "\nassembler working on combination"
 ./Assembler ../test/cnr ../test/machinecode/cnr_machine
 
 cd ..
 
 g++ -o old old.cpp
-
+echo "\nassembler(Old) working on assembly"
 ./old ./test/assembly ./test/machinecode/old_machine
+echo "\nassembler(Old) working on factorial"
 ./old ./test/factorial ./test/machinecode/old_factorial_machine
+echo "\nassembler(Old) working on combination"
 ./old ./test/cnr ./test/machinecode/old_cnr_machine
 
 rm old
