7d4da5c0c14054759abaf0133f8e9bdb2865fef5
Akaifox16 <kamonpat_s@cmu.ac.th>
2022-03-05 01:04:15 +0700

    add old result

--- test.sh
+++ test.sh
@@ -9,3 +9,11 @@ make
 ./Assembler ../test/assembly ../test/machinecode/machine
 ./Assembler ../test/factorial ../test/machinecode/factorial_machine
 ./Assembler ../test/cnr ../test/machinecode/cnr_machine
+
+cd ..
+
+g++ -o old old.cpp
+
+./old ./test/assembly ./test/machinecode/old_machine
+./old ./test/factorial ./test/machinecode/old_factorial_machine
+./old ./test/cnr ./test/machinecode/old_cnr_machine
